45391d7a467c3ba5da60754f8aa3b772ea1725ba
Mariyam Rahman <mariyamrahman38151@gmail.com>
2023-05-20 04:10:55 +0600

    my toys route working

--- index.js
+++ index.js
@@ -19,35 +19,40 @@ app.use(express.json());
 const toyCollection = client.db("brick-city").collection("products");
 
 app.get("/products", async (req, res) => {
-  const per_page = req.query.per_page || 20;
-  const page = req.query.page || 1;
-  const category = req.query.category || null;
   //   give me all products where {category:1}
-  //   const onlyMyProducts = req.query.onlyMyProducts;
-  const cursor = toyCollection.find();
+  const { per_page = 20, page = 1, sellerEmail } = req.query;
+  let cursor;
+  if (sellerEmail) {
+    cursor = toyCollection.find({ sellerEmail });
+  } else {
+    cursor = toyCollection.find();
+  }
   const result = await cursor.toArray();
 
   res.send(result);
 });
 
 app.post("/product", async (req, res) => {
-  const { name, price, stock, description, imageUrl, userId } = req.body;
+  const { name, price, stock, description, imageUrl, sellerEmail, sellerName } =
+    req.body;
   const productObj = {
     name,
     price,
     stock,
     description,
     imageUrl,
-    sellerId: userId,
+    sellerEmail,
+    sellerName,
   };
   const result = await client
     .db("brick-city")
     .collection("products")
     .insertOne(productObj, (err, result) => {});
 
+  console.log("reqbody");
   console.log(req.body);
   console.log(result);
-  res.send("create product");
+  res.send(result);
 });
 
 app.put("/product/:id", async (req, res) => {
